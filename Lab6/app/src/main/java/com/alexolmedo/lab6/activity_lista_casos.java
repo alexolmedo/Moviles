31f247f77b54232bdee3dcc6b4d2fd0ec0c162ad
Alexander Olmedo <olmedo2408@gmail.com>
2018-01-08 08:51:20 -0500

    Ventana Lista de Casos

---
+++ Lab6/app/src/main/java/com/alexolmedo/lab6/activity_lista_casos.java
@@ -0,0 +1,13 @@
+package com.alexolmedo.lab6;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class activity_lista_casos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_casos);
+    }
+}
