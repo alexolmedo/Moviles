eb2133720a1b91c9619058adf5966b6ac83ceabe
Alexander Olmedo <olmedo2408@gmail.com>
2018-01-15 08:21:43 -0500

    Añadir un control WebView para mostrar una página

--- Lab9/app/src/main/java/com/alexolmedo/lab9/ControlWeb.java
+++ Lab9/app/src/main/java/com/alexolmedo/lab9/ControlWeb.java
@@ -2,6 +2,7 @@ package com.alexolmedo.lab9;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class ControlWeb extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class ControlWeb extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_control_web);
+
+        WebView webView = findViewById(R.id.webView);
+        webView.loadUrl("http://fis.epn.edu.ec/");
     }
 }
