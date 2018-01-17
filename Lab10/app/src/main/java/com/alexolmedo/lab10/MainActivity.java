71b44e1b9dc60d224b2653b146ceffeb0fe20c13
Alexander Olmedo <olmedo2408@gmail.com>
2018-01-17 10:09:46 -0500

    Mostrar Date Picker y mostrar fecha

--- Lab10/app/src/main/java/com/alexolmedo/lab10/MainActivity.java
+++ Lab10/app/src/main/java/com/alexolmedo/lab10/MainActivity.java
@@ -1,13 +1,69 @@
 package com.alexolmedo.lab10;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
+    private Button buttonEscoger;
+    private Calendar calendar;
+    private TextView dateView;
+    private int year;
+    private int month;
+    private int day;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonEscoger = findViewById(R.id.buttonEscoger);
+        buttonEscoger.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setDate(buttonEscoger);
+            }
+        });
+        dateView = findViewById(R.id.dateView);
+        calendar = Calendar.getInstance();
+
+        year = calendar.get(Calendar.YEAR);
+        month = calendar.get(Calendar.MONTH);
+        day = calendar.get(Calendar.DAY_OF_MONTH);
+        showDate(year, month+1, day);
+
+    }
+
+    @SuppressWarnings("deprecation")
+    public void setDate(View view) {
+        showDialog(999);
+        Toast.makeText(getApplicationContext(), "Ejemplo de calendario", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        if (id == 999) {
+            return new DatePickerDialog(this, myDateListener, year, month, day);
+        }
+        return null;
+    }
+
+    private DatePickerDialog.OnDateSetListener myDateListener = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+            showDate(year, month+1, dayOfMonth);
+        }
+    };
+
+    private void showDate(int year, int month, int dayOfMonth){
+        dateView.setText(new StringBuilder().append(dayOfMonth).append("/").append(month).append("/").append(year));
     }
 }
