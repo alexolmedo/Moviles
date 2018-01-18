fc8c878278270137daf187f10b188338f2a48c0c
Alexander Olmedo <olmedo2408@gmail.com>
2018-01-18 10:04:54 -0500

    Agregado DatePicker en fechas de inicio y fin

--- Lab6/app/src/main/java/com/alexolmedo/lab6/IngresoCasos.java
+++ Lab6/app/src/main/java/com/alexolmedo/lab6/IngresoCasos.java
@@ -1,29 +1,63 @@
 package com.alexolmedo.lab6;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
+
+import java.util.Calendar;
+import java.util.Locale;
 
 public class IngresoCasos extends AppCompatActivity {
 
     DatabaseHandler db = new DatabaseHandler(this);
 
+    private EditText id;
+    private EditText cliente;
+    private EditText fechaInicio;
+    private EditText fechaFin;
+    private Spinner estado;
+
+    Calendar myCalendar = Calendar.getInstance();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ingreso_casos);
+
+        fechaInicio = findViewById(R.id.editTextFechaInicio);
+        fechaFin = findViewById(R.id.editTextFechaFin);
+
+        fechaInicio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new DatePickerDialog(IngresoCasos.this, myDateListenerStart, myCalendar
+                        .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                        myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+
+        fechaFin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new DatePickerDialog(IngresoCasos.this, myDateListenerEnd, myCalendar
+                        .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                        myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
     }
 
     public void botonIngresarCaso(View view){
-        EditText id = findViewById(R.id.editTextID);
-        EditText cliente = findViewById(R.id.editTextCliente);
-        EditText fechaInicio = findViewById(R.id.editTextFechaInicio);
-        EditText fechaFin = findViewById(R.id.editTextFechaFin);
-        Spinner estado = findViewById(R.id.spinnerEstado);
+        id = findViewById(R.id.editTextID);
+        cliente = findViewById(R.id.editTextCliente);
+        fechaInicio = findViewById(R.id.editTextFechaInicio);
+        fechaFin = findViewById(R.id.editTextFechaFin);
+        estado = findViewById(R.id.spinnerEstado);
         Log.d("Insert", "Datos: "+id.getText()+cliente.getText()+fechaInicio.getText()+fechaFin.getText()+String.valueOf(estado.getSelectedItem()));
         db.addCaso(new Caso(Integer.parseInt(id.getText().toString()), cliente.getText().toString(),fechaInicio.getText().toString(),fechaFin.getText().toString(),String.valueOf(estado.getSelectedItem())));
         Log.d("Insert", "Ingreso Correcto");
@@ -36,4 +70,19 @@ public class IngresoCasos extends AppCompatActivity {
         fechaFin.setText("");
         estado.setSelection(0);
     }
+
+    private DatePickerDialog.OnDateSetListener myDateListenerStart = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+            fechaInicio.setText(new StringBuilder().append(dayOfMonth).append("/").append(month+1).append("/").append(year));
+        }
+    };
+
+    private DatePickerDialog.OnDateSetListener myDateListenerEnd = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+            fechaFin.setText(new StringBuilder().append(dayOfMonth).append("/").append(month+1).append("/").append(year));
+        }
+    };
+
 }
