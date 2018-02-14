4b57c3ad423e7c4c62be1f7f16a6e0be0312adf2
Alexander Olmedo <olmedo2408@gmail.com>
2018-02-14 10:33:48 -0500

    Agregado botón y actividades

---
+++ Lab13/app/src/main/java/com/alexolmedo/lab13/ConsultaContactos.java
@@ -0,0 +1,13 @@
+package com.alexolmedo.lab13;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class ConsultaContactos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_consulta_contactos);
+    }
+}
--- Lab13/app/src/main/java/com/alexolmedo/lab13/MainActivity.java
+++ Lab13/app/src/main/java/com/alexolmedo/lab13/MainActivity.java
@@ -1,13 +1,24 @@
 package com.alexolmedo.lab13;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button botonConsulta;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        botonConsulta = findViewById(R.id.btnConsulta);
+        botonConsulta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), ConsultaContactos.class);
+                startActivity(intent);
+            }
+        });
     }
 }
